053e5edba9f9109d6e96f7a247ce9c25891d6cb7
Dmitry Kragel <dmitry.kragel@gmail.com>
2023-06-02 09:36:39 +0300

    внесены правки, добавлены кнопки, поменяны пилоты и тренеры местами

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,56 @@
 jQuery(function () {
-    jQuery("#slider-promo").slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: "linear",
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        draggable: true,
-    });
+    // Слайдер ПРОМО
+    jQuery("#slider-promo")
+        .slick({
+            autoplay: true,
+            autoplaySpeed: 3500,
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            pauseOnDotsHover: false,
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: "linear",
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            draggable: true,
+        })
+        .on("afterChange", function (slick, currentSlide) {
+            $(".promo-title").toggleClass("even");
+        });
 
+    // Слайдер ЛОДКИ
     jQuery("#slider-boats").slick({
         dots: true,
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
-        draggable: true,
+        arrows: true,
+        draggable: false,
         speed: 0,
+        appendArrows: $('.slider-boats-arrows'),
+        nextArrow: '<span class="slick-next"></span>',
+        prevArrow: '<span class="slick-prev"></span>'
     });
 
+    // Слайдер ПИЛОТЫ
     jQuery("#slider-pilots").slick({
         dots: true,
         infinite: true,
         speed: 0,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        arrows: true,
         appendDots: $(".slider-pilots-dots"),
-        draggable: true,
+        draggable: false,
+        appendArrows: $('.slider-pilots-arrows'),
+        nextArrow: '<span class="slick-next"></span>',
+        prevArrow: '<span class="slick-prev"></span>'
     });
 
+    // Прокрутка страницы от верхнего меню
     jQuery("a[href^='#']").on("click", function (e) {
         e.preventDefault();
         let href = $(this).attr("href");
